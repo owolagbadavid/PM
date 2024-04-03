f18fe8fa7b67950c4bd5d68b11ca9d23f79d1d04
DAVID <owolagbadavid@gmail.com>
2024-04-03 22:07:27 +0100

    update models

--- db/models.py
+++ db/models.py
@@ -1,6 +1,35 @@
 from app import db, bcrypt
 from sqlalchemy.orm import mapped_column, Mapped, relationship
-from sqlalchemy import String, Integer, ForeignKey
+from sqlalchemy import String, Integer, ForeignKey, Column
+
+
+workspace_admin_association_table = db.Table('workspace_admin',
+                                             Column('adminstrator_id', Integer, ForeignKey(
+                                                 'users.id'), primary_key=True),
+                                             Column('workspace_id', Integer, ForeignKey(
+                                                 'workspaces.id'), primary_key=True)
+                                             )
+
+project_manager_association_table = db.Table('project_manager',
+                                             Column('manager_id', Integer, ForeignKey(
+                                                 'users.id'), primary_key=True),
+                                             Column('project_id', Integer, ForeignKey(
+                                                 'projects.id'), primary_key=True)
+                                             )
+
+project_contributor_association_table = db.Table('project_contributor',
+                                                 Column('contibutor_id', Integer, ForeignKey(
+                                                     'users.id'), primary_key=True),
+                                                 Column('project_id', Integer, ForeignKey(
+                                                     'projects.id'), primary_key=True)
+                                                 )
+
+task_assignment_association_table = db.Table('task_assignment',
+                                             Column('assigned_to_id', Integer, ForeignKey(
+                                                 'users.id'), primary_key=True),
+                                             Column('task_id', Integer, ForeignKey(
+                                                 'tasks.id'), primary_key=True)
+                                             )
 
 
 class User(db.Model):
@@ -27,3 +56,46 @@ class User(db.Model):
 
     def check_password_correction(self, attempted_password):
         return bcrypt.check_password_hash(self.password_hash, attempted_password)
+
+
+class WorkSpace(db.Model):
+
+    __tablename__ = 'workspaces'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    administrators = relationship(
+        'User', secondary=workspace_admin_association_table, backref='workspaces')
+
+    projects = relationship('Project', backref='owned_workspace', lazy=True)
+
+
+class Project(db.Model):
+
+    __tablename__ = 'projects'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    managers = relationship(
+        'User', secondary=project_manager_association_table, backref='projects')
+
+    contributors = relationship(
+        'User', secondary=project_contributor_association_table, backref='projects')
+
+    workspace_id: Mapped[int] = mapped_column(
+        ForeignKey('workspaces.id'), nullable=True)
+
+    tasks = relationship('Task', backref='owned_project', lazy=True)
+
+
+class Task(db.Model):
+
+    __tablename__ = 'tasks'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    project_id: Mapped[int] = mapped_column(
+        ForeignKey('projects.id'), nullable=True)
+
+    assigned_to = relationship(
+        'User', secondary=task_assignment_association_table, backref='tasks')
