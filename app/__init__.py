7d1dea49979b4f009950b88fb3d806ce997f969a
DAVID <owolagbadavid@gmail.com>
2024-04-20 11:08:16 +0100

    cors

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,11 @@ from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from werkzeug.exceptions import HTTPException
+from flask_cors import CORS
 
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://db:5432/pm?user=postgres"
 app.config['SECRET_KEY'] = '87ddc7dc2ca2f007ee13dca6'
